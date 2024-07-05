e3c6a66237c4ffbbb6164a0fecccbcdac82d4ac5
Manoah <72307123+Manfox177@users.noreply.github.com>
2024-07-05 20:31:52 +0200

    Update LLaMA3-V_finetune_lora.sh

--- scripts/LLaMA3-V_finetune_lora.sh
+++ scripts/LLaMA3-V_finetune_lora.sh
@@ -5,18 +5,18 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct \
     --version llama3 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path /kaggle/working/llava_instruct_150k.json \
+    --image_folder /kaggle/input/cocodataset/train2017 \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-8b-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter /checkpoints/brainbustergpt-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-8b-finetune-lora \
+    --bf16 False \
+    --output_dir ./checkpoints/brainbustergpt-finetune-lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -30,7 +30,7 @@ deepspeed llava/train/train_mem.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 4096 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
