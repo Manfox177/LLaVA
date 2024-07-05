23f43016b923b7c17180eaac2462a0d33ee11ca5
Manoah <72307123+Manfox177@users.noreply.github.com>
2024-07-05 20:22:03 +0200

    Update LLaMA3-V_pretrain.sh

--- scripts/LLaMA3-V_pretrain.sh
+++ scripts/LLaMA3-V_pretrain.sh
@@ -4,8 +4,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct \
     --version plain \
-    --data_path ./playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
-    --image_folder ./playground/data/LLaVA-Pretrain/images \
+    --data_path /kaggle/working/llava_instruct_150k.json \
+    --image_folder /kaggle/input/cocodataset/train2017 \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
@@ -13,7 +13,7 @@ deepspeed llava/train/train_mem.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 False \
-    --output_dir ./checkpoints/llava-v1.5-8b-pretrain \
+    --output_dir ./checkpoints/brainbustergpt-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
